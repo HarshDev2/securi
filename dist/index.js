e16b4000d8f20d661b6a6d1e1b8985de3fc8e352
Harsh Agrawal <averageworkspace@gmail.com>
2023-02-02 20:37:38 +0530

    added password generator

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
-"use strict";
-console.log('Hello World');
+import { argv } from 'node:process';
+import inquirer from 'inquirer';
+import generator from 'generate-password-ts';
+let input = argv[2];
+if (input === 'generate') {
+    const response = await inquirer.prompt([{
+            name: 'What should be length of your password?',
+            type: 'list',
+            choices: [6, 8, 10, 12, 16, 20]
+        }
+    ]);
+    const password = generator.generate({
+        length: response['What should be length of your password?'],
+        numbers: true
+    });
+    console.log(`Your password is ${password}`);
+}
+else if (!argv[2])
+    console.log(`Please enter a command.`);
+else
+    console.error((`Unidentified Command: ${input}`));
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
 #!/usr/bin/env node
-console.log('Hello World')
+import { argv } from 'node:process';
+import inquirer from 'inquirer';
+import generator from 'generate-password-ts';
+
+let input: String = argv[2];
+
+if (input === 'generate') {
+    const response = await inquirer.prompt([{
+            name: 'What should be length of your password?',
+            type: 'list',
+            choices: [6, 8, 10, 12, 16, 20]
+        }
+    ]);
+
+    const password = generator.generate({
+        length: response['What should be length of your password?'],
+        numbers: true
+    });
+    
+    console.log(`Your password is ${password}`);
+}
+
+else if(!argv[2]) console.log(`Please enter a command.`)
+else console.error((`Unidentified Command: ${input}`));
