0838cd8dbdb4b93316d4dc88f051d39ef8b30a38
Harsh Agrawal <averageworkspace@gmail.com>
2023-02-03 07:07:28 +0530

    Look Improved

--- dist/index.js
+++ dist/index.js
@@ -4,19 +4,27 @@ import inquirer from 'inquirer';
 import generator from 'generate-password-ts';
 let input = argv[2];
 if (input === 'generate') {
+    const response1 = await inquirer.prompt([{
+            name: 'Name your password!',
+            type: 'input',
+        }]);
     const response = await inquirer.prompt([{
             name: 'What should be length of your password?',
             type: 'list',
             choices: [6, 8, 10, 12, 16, 20]
         }
     ]);
+    const name = response1['Name your password!'];
     const password = generator.generate({
         length: response['What should be length of your password?'],
         numbers: true
     });
-    console.log(`Your password is ${password}`);
+    console.log(`Your password is ${name}: ${password}`);
+}
+else if (input === 'view') {
+    console.log('View is being built.');
 }
 else if (!argv[2])
-    console.log(`Please enter a command.`);
+    console.log(`\t \t Securi CLI v0.0.1 \nCommands-\ngenerate - generates a random password and saves it.\nview - display all your saved passwords.`);
 else
     console.error((`Unidentified Command: ${input}`));
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ import generator from 'generate-password-ts';
 let input: String = argv[2];
 
 if (input === 'generate') {
+    const response1 = await inquirer.prompt([{
+        name: 'Name your password!',
+        type: 'input',
+    }]);
+
     const response = await inquirer.prompt([{
             name: 'What should be length of your password?',
             type: 'list',
@@ -13,13 +18,20 @@ if (input === 'generate') {
         }
     ]);
 
+    const name = response1['Name your password!']
+
     const password = generator.generate({
         length: response['What should be length of your password?'],
         numbers: true
     });
     
-    console.log(`Your password is ${password}`);
+    console.log(`Your password is ${name}: ${password}`);
 }
 
-else if(!argv[2]) console.log(`Please enter a command.`)
+else if (input === 'view') {
+    console.log('View is being built.')
+}
+
+else if(!argv[2]) console.log(`\t \t Securi CLI v0.0.1 \nCommands-\ngenerate - generates a random password and saves it.\nview - display all your saved passwords.`)
+
 else console.error((`Unidentified Command: ${input}`));
